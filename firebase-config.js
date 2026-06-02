2bb6139a041bedf0ebbb65420e9213d282ca537b
Fagundes7828 <mateusfagundesbackup@gmail.com>
2026-06-02 14:48:36 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+ 
 const firebaseConfig = {
-  apiKey: "AIzaSyAP2GJ1DqnBHAHyb7FaSPujAGJU6pmd7JE",
-  authDomain: "mf-agenda.firebaseapp.com",
-  projectId: "mf-agenda",
-  storageBucket: "mf-agenda.firebasestorage.app",
+  apiKey:            "AIzaSyAP2GJ1DqnBHAHyb7FaSPujAGJU6pmd7JE",
+  authDomain:        "mf-agenda.firebaseapp.com",
+  projectId:         "mf-agenda",
+  storageBucket:     "mf-agenda.firebasestorage.app",
   messagingSenderId: "963529011516",
-  appId: "1:963529011516:web:4bba670194382b1ec9ee39",
-  measurementId: "G-DMYEFR6QDN"
+  appId:             "1:963529011516:web:4bba670194382b1ec9ee39",
+  measurementId:     "G-DMYEFR6QDN"
 };
-
-// Initialize Firebase
+ 
+// Inicializa o Firebase e exporta o que o resto do app vai usar
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
+ 
